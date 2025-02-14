a93ca89d2f28510a869d162612f7c8edca55d20e
moink <theresa.robinson@gmail.com>
2025-02-14 20:25:47 +0100

    Add anchor constraints

--- running_order.py
+++ running_order.py
@@ -24,11 +24,13 @@ class SketchOrder:
     order: list[int]
 
     def __init__(self, sketches):
-        print(sketches, type(sketches))
         self.order = list(sketches)
 
     def __hash__(self):
         return hash(tuple(self.order))
+
+    def __repr__(self):
+        return f"Sketch Order: {','.join(str(sketch) for sketch in self.order)}"
 
     def __eq__(self, other):
         if len(self.order) != len(other.order):
@@ -38,15 +40,29 @@ class SketchOrder:
     def is_full_length(self, num_sketches):
         return len(self.order) == num_sketches
 
-    def possible_next_states(self, allowed_nexts, num_sketches):
+    def possible_next_states(self, allowed_nexts, num_sketches, anchors=None):
+        if anchors is None:
+            anchors = {}
         if not self.order:
-            return set(SketchOrder([first_sketch]) for first_sketch in range(num_sketches))
+            try:
+                return {SketchOrder([anchors[0]])}
+            except KeyError:
+                return set(SketchOrder([first_sketch]) for first_sketch in range(num_sketches))
         last_sketch = self.order[-1]
-        allowed_next = allowed_nexts[last_sketch].difference(self.order)
-        return {SketchOrder(self.order + [next_sketch]) for next_sketch in allowed_next}
+        try:
+            next_sketch = anchors[len(self.order)]
+        except KeyError:
+            allowed_next = allowed_nexts[last_sketch].difference(self.order)
+            return {SketchOrder(self.order + [next_sketch]) for next_sketch in allowed_next}
+        else:
+            if next_sketch in allowed_nexts[last_sketch] and next_sketch not in self.order:
+                return {SketchOrder(self.order + [next_sketch])}
+            else:
+                return set()
 
 
-def find_all_orders(allowed_nexts):
+
+def find_all_orders(allowed_nexts, anchors=None):
     num_sketches = len(allowed_nexts)
     allowed_full_orders = set()
     empty_order = SketchOrder([])
@@ -54,7 +70,7 @@ def find_all_orders(allowed_nexts):
     discovered = {empty_order}
     while stack:
         partial_order = stack.pop()
-        candidates = partial_order.possible_next_states(allowed_nexts, num_sketches)
+        candidates = partial_order.possible_next_states(allowed_nexts, num_sketches, anchors)
         for candidate_one_longer in candidates:
             if candidate_one_longer.is_full_length(num_sketches):
                 allowed_full_orders.add(candidate_one_longer)
--- test_running_order.py
+++ test_running_order.py
@@ -5,7 +5,7 @@ import running_order
 
 class TestGetAllowableNextSketches(unittest.TestCase):
     def test_three_sketches(self):
-        sketch1 = running_order.Sketch("Jedi Warrior", {"Adrian", "Richie", "Michelle"})
+        sketch1 = running_order.Sketch("Jedi Warrior", {"Adrian", "Richie", "Michele"})
         sketch2 = running_order.Sketch("I am the boss", {"Theresa", "Rocio"})
         sketch3 = running_order.Sketch("It's just me", {"Adrian"})
         result = running_order.get_allowable_next_sketches([sketch1, sketch2, sketch3])
@@ -34,6 +34,54 @@ class TestSketchOrder(unittest.TestCase):
         order2 = running_order.SketchOrder([0, 1, 2])
         self.assertNotEqual(order1, order2)
 
+    def test_possible_next_states(self):
+        order = running_order.SketchOrder([1, 0])
+        allowed_nexts = {
+            0: {1, 2, 3},
+            1: {0, 2},
+            2: {1},
+        }
+        expected_result = {
+            running_order.SketchOrder([1, 0, 2]),
+            running_order.SketchOrder([1, 0, 3])
+        }
+        result = order.possible_next_states(allowed_nexts, 3)
+        self.assertEqual(expected_result, result)
+
+    def test_possible_first_with_anchor(self):
+        order = running_order.SketchOrder([])
+        anchors = {0: 1}
+        allowed_nexts = {
+            0: {1, 2, 3},
+            1: {0, 2},
+            2: {1},
+        }
+        expected_result = {
+            running_order.SketchOrder([1])
+        }
+        result = order.possible_next_states(allowed_nexts, 3, anchors)
+        self.assertEqual(expected_result, result)
+
+    def test_possible_mid_with_anchor(self):
+        anchors = {2: 0}
+        order = running_order.SketchOrder([1, 2])
+        allowed_nexts = {2: {0, 3}}
+        expected_result = {
+            running_order.SketchOrder([1, 2, 0])
+        }
+        result = order.possible_next_states(allowed_nexts, 4, anchors)
+        self.assertEqual(expected_result, result)
+
+    def test_impossible_mid_with_anchor(self):
+        anchors = {2: 0}
+        order = running_order.SketchOrder([1, 2])
+        allowed_nexts = {2: {1, 3}}
+        expected_result = set()
+        result = order.possible_next_states(allowed_nexts, 4, anchors)
+        self.assertEqual(expected_result, result)
+
+
+class TestFindAllOrders(unittest.TestCase):
     def test_find_all_orders(self):
         allowed_nexts = {
             0: {1},
@@ -47,6 +95,31 @@ class TestSketchOrder(unittest.TestCase):
         result = running_order.find_all_orders(allowed_nexts)
         self.assertEqual(expected_result, result)
 
+    def test_find_all_orders_with_start_anchor(self):
+        allowed_nexts = {
+            0: {1},
+            1: {0, 2},
+            2: {1},
+        }
+        anchors = {0: 0}
+        expected_result = {
+            running_order.SketchOrder([0, 1, 2]),
+        }
+        result = running_order.find_all_orders(allowed_nexts, anchors)
+        self.assertEqual(expected_result, result)
+
+    def test_find_all_orders_with_end_anchor(self):
+        allowed_nexts = {
+            0: {1},
+            1: {0, 2},
+            2: {1},
+        }
+        anchors = {2: 0}
+        expected_result = {
+            running_order.SketchOrder([2, 1, 0]),
+        }
+        result = running_order.find_all_orders(allowed_nexts, anchors)
+        self.assertEqual(expected_result, result)
 
 if __name__ == '__main__':
     unittest.main()
